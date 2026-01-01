341c22578ede5ea174c0a858560810428440af41
Rick Gaiser <rgaiser@gmail.com>
2026-01-01 16:20:29 +0100

    r5900 add MIPS IV supported instr. - fix

--- llvm/test/MC/Mips/r5900-mips4.s
+++ llvm/test/MC/Mips/r5900-mips4.s
@@ -2,7 +2,8 @@
 # RUN:   | FileCheck %s
 
 # Test R5900 MIPS IV instructions
-# The R5900 supports a subset of MIPS IV: MOVN, MOVZ, PREF, and related FPU moves
+# The R5900 supports a subset of MIPS IV: MOVN, MOVZ, PREF
+# Note: R5900 does NOT support FPU conditional moves (movn.s, movz.s, etc.)
 
 # Integer conditional moves
 # CHECK: movn    $12, $13, $14           # encoding: [0x0b,0x60,0xae,0x01]
@@ -16,19 +17,6 @@ movz    $t0, $t1, $t2
 
 # CHECK: movz    $2, $3, $4              # encoding: [0x0a,0x10,0x64,0x00]
 movz    $v0, $v1, $a0
-
-# FPU conditional moves (single-precision only on R5900)
-# CHECK: movn.s  $f0, $f1, $12           # encoding: [0x13,0x08,0x0c,0x46]
-movn.s  $f0, $f1, $t0
-
-# CHECK: movn.s  $f2, $f3, $13           # encoding: [0x93,0x18,0x0d,0x46]
-movn.s  $f2, $f3, $t1
-
-# CHECK: movz.s  $f4, $f5, $14           # encoding: [0x12,0x29,0x0e,0x46]
-movz.s  $f4, $f5, $t2
-
-# CHECK: movz.s  $f6, $f7, $4            # encoding: [0x92,0x39,0x04,0x46]
-movz.s  $f6, $f7, $a0
 
 # Prefetch instruction
 # CHECK: pref    0, 0($12)               # encoding: [0x00,0x00,0x80,0xcd]
